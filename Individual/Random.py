4d206ab7b4d24b676dfb4371ce5fc3250e9deb2b
Rohan Raj Poudel <poudelrohan58@gmail.com>
2024-03-01 20:16:53 +0545

    Random.py good to go

--- Individual/Random.py
+++ Individual/Random.py
@@ -1,13 +1,80 @@
 import cv2
 import torch
-import sys
+import os
+import random
+from tensorflow.keras.models import load_model
 
+# Function to perform OCR on an image using the trained model
+def perform_ocr(image, ocrModel):
+    try:
+        image = cv2.resize(image, (32, 32))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Bilateral filtering
+        image = cv2.bilateralFilter(image,5,30,45)
+        # Otsu's thresholding
+        _,image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # thinning the image
+        image = cv2.ximgproc.thinning(image)
 
+        # Reshape the image to match the input shape expected by the model
+        image = image.reshape(1, 32, 32, 1)
 
-def calc_and_show_result(image_path, model, classes, i, files_length):
+        # Use the model to predict the character
+        prediction = ocrModel.predict(image)
+        # Get the index of the predicted class
+        predicted_class = prediction.argmax()
+
+        # Convert the predicted class index to the corresponding character
+        # This assumes that your classes are labeled with integer values corresponding to characters
+        predicted_character = str(predicted_class)
+        match predicted_character:
+            case "10":
+                predicted_character = "Baa"
+            case "11":
+                predicted_character = "Cha"
+            case "12":
+                predicted_character = "Pa"
+            case "13":
+                predicted_character = "Ga"
+        return predicted_character
+    except:
+        pass
+
+def charSegShow(image, model, classes, ocrModel):
+    image = cv2.resize(image,(400,200))
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_tensor = torch.tensor(image_rgb / 255.0, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
+
+    # Perform inference
+    with torch.no_grad():
+        predictions = model(image_tensor)
+
+    # Extract relevant information from predictions
+    boxes = predictions[0]['boxes']
+    scores = predictions[0]['scores']
+    labels = predictions[0]['labels']
+
+    # Draw bounding boxes for "Char" class
+    j = 0
+    for box, score, label in zip(boxes, scores, labels):
+        j += 1
+        if classes[label] == "Char" and score > 0.8:  # Adjust the confidence threshold as needed
+            box = [int(coord) for coord in box.tolist()]
+            # image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+            #to save the cropped image
+            # print(box)
+            cropped_image = image[(box[1]-4):(box[3]+4), (box[0]-2):(box[2]+2)]
+            character = perform_ocr(cropped_image, ocrModel)
+            # cv2.imwrite(f"./RedLP_{i}.jpg", cropped_image) 
+            # image = cv2.putText(image, f"{score:.2f}", (box[0], box[1]),
+            image = cv2.rectangle(image, (box[0], box[1]-4), (box[2], box[3]+4), (0, 255, 0), 2)
+            image = cv2.putText(image, character, (box[0]+4, box[1]+13),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    return image
+
+def numPlateShow(image_path, model, classes):
     image = cv2.imread(image_path)
     h,w,_=image.shape
-    image = cv2.resize(image,(int(w/1.5),int(h/1.5)))
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image_tensor = torch.tensor(image_rgb / 255.0, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
 
@@ -24,32 +91,44 @@ def calc_and_show_result(image_path, model, classes, i, files_length):
     for box, score, label in zip(boxes, scores, labels):
         if classes[label] == "RedLP" and score > 0.8:  # Adjust the confidence threshold as needed
             box = [int(coord) for coord in box.tolist()]
-            # image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
-            #to save the cropped image
-            # print(box)
+            # Corp the number plate
             cropped_image = image[(box[1]):(box[3]), (box[0]):(box[2])]
-            cv2.imwrite(f"../Our Dataset/Working Dataset/Num Plate/NumPlt_{i}.jpg", cropped_image) 
-            cv2.imwrite(f"../Our Dataset/Working Dataset/Still Vehicle/Still_{i}.jpg", image) 
-            # image = cv2.putText(image, f"{classes[label]}: {score:.2f}", (box[0]-5, box[1] - 10),
-            # image = cv2.putText(image, f"LP: {score:.2f}", (box[0]-5, box[1] - 10),
-            # image = cv2.putText(image, f"Lic Plt:{box[0]-5} {box[1]-10} {score:.2f}", (box[0], box[1] - 5),
-                                # cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    # Display the result using cv2.imshow
-    # cv2.imshow(str(i), image)
-    i+=1
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    if cv2.waitKey(33) == 27 or i==files_length:
-        sys.exit()
-    return i
+            # Draw rectangle
+            image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+            image = cv2.putText(image, f"LP: {score:.2f}", (box[0]-5, box[1] - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            
+    image = cv2.resize(image,(int(w/1.5),int(h/1.5)))
+    return image, cropped_image
 
 def main():
     # Define the path to Faster RCNN Number Plate Detection Model
     numPlateModelPath = './Bulk/numberPlate-FasterRCNN.pth'
+    charSegModelPath = './Bulk/characterSegment-FasterRCNN.pth'
+    ocrModelPath = './Bulk/ocrModel.h5'
     # Load the model
     numPlateModel = torch.load(numPlateModelPath, map_location=torch.device('cpu'))
+    charSegModel = torch.load(charSegModelPath, map_location=torch.device('cpu'))
+    ocrModel = load_model(ocrModelPath)
     # Set the model to evaluation mode
     numPlateModel.eval()  
+    charSegModel.eval()  
     # Define the class names
-    numPlateclasses = ["RedLP", "RedLP"]  # Adjust the class names based on your dataset
+    numPlateClasses = ["RedLP", "RedLP"]
+    charSegClasses = ["Char", "Char"]
+    dataset_path="C:/Users/rohan/Desktop/Now/MP model/Our Dataset/Working Dataset/Still Vehicle/"
+    os.makedirs(dataset_path, exist_ok=True)
+    all_files = os.listdir(dataset_path)
+    jpg_files = [file for file in all_files if file.lower().endswith('.jpg')]
+    files_length=len(jpg_files)-1
+    jpg_file = jpg_files[random.randint(0, files_length)]
+    originalImage, numPlateImage = numPlateShow(dataset_path+jpg_file, numPlateModel, numPlateClasses)
+    finalResultImage = charSegShow(numPlateImage, charSegModel, charSegClasses, ocrModel)
+
+    cv2.imshow("Original Image", originalImage)
+    cv2.imshow("Final Result", finalResultImage)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+main()
