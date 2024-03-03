9d2e5604bbc46ace26da49fdcd2b3a2078ea256a
Rohan Raj Poudel <poudelrohan58@gmail.com>
2024-03-03 20:28:52 +0545

    random with camera

---
+++ Individual/randomWithCamera.py
@@ -0,0 +1,157 @@
+import cv2
+import torch
+import os
+import random
+from tensorflow.keras.models import load_model
+
+# Function to perform OCR on an image using the trained model
+def perform_ocr(image, ocrModel):
+    try:
+        image = cv2.resize(image, (32, 32))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Bilateral filtering
+        image = cv2.bilateralFilter(image,5,30,45)
+        # Otsu's thresholding
+        _,image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # thinning the image
+        image = cv2.ximgproc.thinning(image)
+
+        # Reshape the image to match the input shape expected by the model
+        image = image.reshape(1, 32, 32, 1)
+
+        # Use the model to predict the character
+        prediction = ocrModel.predict(image)
+        # Get the index of the predicted class
+        predicted_class = prediction.argmax()
+
+        # Convert the predicted class index to the corresponding character
+        # This assumes that your classes are labeled with integer values corresponding to characters
+        predicted_character = str(predicted_class)
+        match predicted_character:
+            case "10":
+                predicted_character = "Baa"
+            case "11":
+                predicted_character = "Cha"
+            case "12":
+                predicted_character = "Pa"
+            case "13":
+                predicted_character = "Ga"
+        return predicted_character
+    except:
+        pass
+
+def charSegShow(image, model, classes, ocrModel):
+    image = cv2.resize(image,(400,200))
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_tensor = torch.tensor(image_rgb / 255.0, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
+
+    # Perform inference
+    with torch.no_grad():
+        predictions = model(image_tensor)
+
+    # Extract relevant information from predictions
+    boxes = predictions[0]['boxes']
+    scores = predictions[0]['scores']
+    labels = predictions[0]['labels']
+
+    # Draw bounding boxes for "Char" class
+    j = 0
+    for box, score, label in zip(boxes, scores, labels):
+        j += 1
+        if classes[label] == "Char" and score > 0.8:  # Adjust the confidence threshold as needed
+            box = [int(coord) for coord in box.tolist()]
+            # image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+            #to save the cropped image
+            # print(box)
+            cropped_image = image[(box[1]-4):(box[3]+4), (box[0]-2):(box[2]+2)]
+            character = perform_ocr(cropped_image, ocrModel)
+            # cv2.imwrite(f"./RedLP_{i}.jpg", cropped_image) 
+            # image = cv2.putText(image, f"{score:.2f}", (box[0], box[1]),
+            image = cv2.rectangle(image, (box[0], box[1]-4), (box[2], box[3]+4), (0, 255, 0), 2)
+            image = cv2.putText(image, character, (box[0]+4, box[1]+13),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    return image
+
+def numPlateShow(image, model, classes):
+    # image = cv2.imread(image_path)
+    h,w,_=image.shape
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_tensor = torch.tensor(image_rgb / 255.0, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
+
+    # Perform inference
+    with torch.no_grad():
+        predictions = model(image_tensor)
+
+    # Extract relevant information from predictions
+    boxes = predictions[0]['boxes']
+    scores = predictions[0]['scores']
+    labels = predictions[0]['labels']
+
+    # Draw bounding boxes for "Red LP" class
+    for box, score, label in zip(boxes, scores, labels):
+        if classes[label] == "RedLP" and score > 0.8:  # Adjust the confidence threshold as needed
+            box = [int(coord) for coord in box.tolist()]
+            # Corp the number plate
+            cropped_image = image[(box[1]):(box[3]), (box[0]):(box[2])]
+            # Draw rectangle
+            image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+            image = cv2.putText(image, f"LP: {score:.2f}", (box[0]-5, box[1] - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            
+    image = cv2.resize(image,(int(w/1.5),int(h/1.5)))
+    try:
+        return cropped_image
+    except:
+        return image
+
+def main():
+    # Define the path to Faster RCNN Number Plate Detection Model
+    numPlateModelPath = './Bulk/numberPlate-FasterRCNN.pth'
+    charSegModelPath = './Bulk/characterSegment-FasterRCNN.pth'
+    ocrModelPath = './Bulk/ocrModel.h5'
+    # Load the model
+    numPlateModel = torch.load(numPlateModelPath, map_location=torch.device('cpu'))
+    charSegModel = torch.load(charSegModelPath, map_location=torch.device('cpu'))
+    ocrModel = load_model(ocrModelPath)
+    # Set the model to evaluation mode
+    numPlateModel.eval()  
+    charSegModel.eval()  
+    # Define the class names
+    numPlateClasses = ["RedLP", "RedLP"]
+    charSegClasses = ["Char", "Char"]
+    dataset_path="C:/Users/rohan/Desktop/Now/MP model/Our Dataset/Working Dataset/Still Vehicle/"
+    os.makedirs(dataset_path, exist_ok=True)
+    all_files = os.listdir(dataset_path)
+    jpg_files = [file for file in all_files if file.lower().endswith('.jpg')]
+    files_length=len(jpg_files)-1
+    jpg_file = jpg_files[random.randint(0, files_length)]
+    originalImage = capture_image()
+    numPlateImage = numPlateShow(originalImage, numPlateModel, numPlateClasses)
+    finalResultImage = charSegShow(numPlateImage, charSegModel, charSegClasses, ocrModel)
+
+    cv2.imshow("Original Image", originalImage)
+    cv2.imshow("Final Result", finalResultImage)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def capture_image():
+    # Open a connection to the webcam (0 indicates the default camera)
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        return
+
+    # Capture a single frame
+    ret, frame = cap.read()
+
+    # Release the webcam
+    cap.release()
+
+    if ret:
+        return frame
+    else:
+        print("Error: Could not capture an image.")
+
+main()
+
